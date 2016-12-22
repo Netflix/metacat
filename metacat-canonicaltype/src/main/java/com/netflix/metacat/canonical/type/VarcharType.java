a4474d8a6c2e11464c1d217ac4addc0c41b70246
Zhen Li <zhenl@netflix.com>
2016-12-21 20:12:45 -0800

    adding array and varchar type

---
+++ metacat-canonicaltype/src/main/java/com/netflix/metacat/canonical/type/VarcharType.java
@@ -0,0 +1,40 @@
+package com.netflix.metacat.canonical.type;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.google.common.collect.Lists;
+import lombok.Getter;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.netflix.metacat.canonical.type.TypeSignature.parseTypeSignature;
+
+/**
+ * Created by zhenli on 12/21/16.
+ */
+public class VarcharType extends AbstractType {
+    /** Default varchar type. */
+    public static final VarcharType VARCHAR = new VarcharType(1);
+
+    @Getter private final int length;
+
+    private VarcharType(final int length) {
+        super(
+            new TypeSignature(
+                Base.VARCHAR.getBaseTypeDisplayName(), Lists.newArrayList(),
+                Collections.singletonList((long) length)));
+
+        if (length < 0) {
+            throw new IllegalArgumentException("Invalid VARCHAR length " + length);
+        }
+        this.length = length;
+    }
+    /**
+     * Cretes varchar type.
+     * @param length length
+     * @return VarcharType
+     */
+    public static VarcharType createVarcharType(final int length) {
+        return new VarcharType(length);
+    }
+}
