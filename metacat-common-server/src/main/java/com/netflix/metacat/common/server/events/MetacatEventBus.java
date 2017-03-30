6bf767ce7fc92d412429d526c0a2350b357d0737
Tom Gianos <tgianos@users.noreply.github.com>
2017-03-30 15:09:29 -0700

    Adding logging and metrics for event subscribers. Adding metrics to the event bus (#84)

--- metacat-common-server/src/main/java/com/netflix/metacat/common/server/events/MetacatEventBus.java
+++ metacat-common-server/src/main/java/com/netflix/metacat/common/server/events/MetacatEventBus.java
@@ -21,51 +21,67 @@ import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.Inject;
+import com.netflix.metacat.common.monitoring.CounterWrapper;
 import com.netflix.metacat.common.server.Config;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Event bus.
  */
+@Slf4j
 public class MetacatEventBus {
     private final AsyncEventBus asyncEventBus;
     private final EventBus syncEventBus;
+    private final ExecutorService executor;
 
     /**
      * Constructor.
+     *
      * @param config config
      */
     @Inject
     public MetacatEventBus(final Config config) {
         final ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("metacat-event-pool-%d").build();
         final int threadCount = config.getEventBusThreadCount();
+        this.executor = Executors.newFixedThreadPool(threadCount, threadFactory);
         this.asyncEventBus = new AsyncEventBus(
             "metacat-async-event-bus",
-            Executors.newFixedThreadPool(threadCount, threadFactory)
+            this.executor
         );
         this.syncEventBus = new EventBus("metacat-sync-event-bus");
     }
 
     /**
      * Post event asynchronously.
+     *
      * @param event event
      */
     public void postAsync(final Object event) {
+        log.debug("Received request to post an event {} asynchronously", event);
+        CounterWrapper.incrementCounter("metacat.events.async");
         this.asyncEventBus.post(event);
     }
 
     /**
      * Post event synchronously.
+     *
      * @param event event
      */
     public void postSync(final Object event) {
+        log.debug("Received request to post an event {} synchronously", event);
+        CounterWrapper.incrementCounter("metacat.events.sync");
         this.syncEventBus.post(event);
     }
 
     /**
      * Registers an object.
+     *
      * @param object object
      */
     public void register(final Object object) {
@@ -75,10 +91,36 @@ public class MetacatEventBus {
 
     /**
      * De-registers an object.
+     *
      * @param object object
      */
     public void unregister(final Object object) {
         asyncEventBus.unregister(object);
         syncEventBus.unregister(object);
+    }
+
+    /**
+     * Shut down the executor. Taken from the javadoc for {@code ExecutorService}
+     *
+     * @see ExecutorService
+     */
+    @PreDestroy
+    public void shutdown() {
+        this.executor.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!this.executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                this.executor.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!this.executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Event bus async thread executor did not terminate");
+                }
+            }
+        } catch (final InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            this.executor.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
     }
 }
--- metacat-main/src/main/java/com/netflix/metacat/main/services/search/MetacatEventHandlers.java
+++ metacat-main/src/main/java/com/netflix/metacat/main/services/search/MetacatEventHandlers.java
@@ -20,6 +20,7 @@ import com.netflix.metacat.common.dto.DatabaseDto;
 import com.netflix.metacat.common.dto.PartitionDto;
 import com.netflix.metacat.common.dto.TableDto;
 import com.netflix.metacat.common.json.MetacatJsonLocator;
+import com.netflix.metacat.common.monitoring.CounterWrapper;
 import com.netflix.metacat.common.server.events.MetacatCreateDatabasePostEvent;
 import com.netflix.metacat.common.server.events.MetacatCreateMViewPostEvent;
 import com.netflix.metacat.common.server.events.MetacatCreateTablePostEvent;
@@ -62,6 +63,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatCreateDatabasePostEventHandler(final MetacatCreateDatabasePostEvent event) {
+        log.debug("Received CreateDatabaseEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.database.create");
         final DatabaseDto dto = event.getDatabase();
         final ElasticSearchDoc doc = new ElasticSearchDoc(dto.getName().toString(), dto,
             event.getRequestContext().getUserName(), false);
@@ -74,6 +77,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatCreateMViewPostEventHandler(final MetacatCreateMViewPostEvent event) {
+        log.debug("Received CreateViewEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.view.create");
         final TableDto dto = event.getTable();
         final ElasticSearchDoc doc = new ElasticSearchDoc(dto.getName().toString(), dto,
             event.getRequestContext().getUserName(), false);
@@ -86,6 +91,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatCreateTablePostEventHandler(final MetacatCreateTablePostEvent event) {
+        log.debug("Received CreateTableEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.create");
         final TableDto dto = event.getTable();
         final ElasticSearchDoc doc = new ElasticSearchDoc(dto.getName().toString(), dto,
             event.getRequestContext().getUserName(), false);
@@ -98,6 +105,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatDeleteDatabasePostEventHandler(final MetacatDeleteDatabasePostEvent event) {
+        log.debug("Received DeleteDatabaseEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.database.delete");
         final DatabaseDto dto = event.getDatabase();
         es.softDelete(ElasticSearchDoc.Type.database.name(), dto.getName().toString(), event.getRequestContext());
     }
@@ -108,6 +117,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatDeleteMViewPostEventHandler(final MetacatDeleteMViewPostEvent event) {
+        log.debug("Received DeleteViewEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.view.delete");
         final TableDto dto = event.getTable();
         es.softDelete(ElasticSearchDoc.Type.mview.name(), dto.getName().toString(), event.getRequestContext());
     }
@@ -118,6 +129,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatDeleteTablePostEventHandler(final MetacatDeleteTablePostEvent event) {
+        log.debug("Received DeleteTableEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.delete");
         final TableDto dto = event.getTable();
         if (MViewServiceImpl.VIEW_DB_NAME.equals(dto.getName().getDatabaseName())) {
             es.softDelete(ElasticSearchDoc.Type.mview.name(), dto.getName().toString(), event.getRequestContext());
@@ -139,6 +152,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatDeleteMViewPartitionPostEventHandler(final MetacatDeleteMViewPartitionPostEvent event) {
+        log.debug("Received DeleteViewPartitionEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.view.partition.delete");
         final List<String> partitionIds = event.getPartitionIds();
         final List<String> esPartitionIds = partitionIds.stream()
             .map(partitionId -> event.getName().toString() + "/" + partitionId).collect(Collectors.toList());
@@ -151,6 +166,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatDeleteTablePartitionPostEventHandler(final MetacatDeleteTablePartitionPostEvent event) {
+        log.debug("Received DeleteTablePartitionEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.partition.delete");
         final List<String> partitionIds = event.getPartitionIds();
         final List<String> esPartitionIds = partitionIds.stream()
             .map(partitionId -> event.getName().toString() + "/" + partitionId).collect(Collectors.toList());
@@ -163,6 +180,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatRenameTablePostEventHandler(final MetacatRenameTablePostEvent event) {
+        log.debug("Received RenameTableEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.rename");
         es.delete(ElasticSearchDoc.Type.table.name(), event.getName().toString());
 
         final TableDto dto = event.getCurrentTable();
@@ -177,6 +196,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatUpdateMViewPostEventHandler(final MetacatUpdateMViewPostEvent event) {
+        log.debug("Received UpdateViewEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.view.update");
         final TableDto dto = event.getTable();
         final ElasticSearchDoc doc = new ElasticSearchDoc(dto.getName().toString(), dto,
             event.getRequestContext().getUserName(), false);
@@ -189,6 +210,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatUpdateTablePostEventHandler(final MetacatUpdateTablePostEvent event) {
+        log.debug("Received UpdateTableEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.update");
         final TableDto dto = event.getCurrentTable();
         final ElasticSearchDoc doc = new ElasticSearchDoc(dto.getName().toString(), dto,
             event.getRequestContext().getUserName(), false);
@@ -212,6 +235,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatSaveMViewPartitionPostEventHandler(final MetacatSaveMViewPartitionPostEvent event) {
+        log.debug("Received SaveViewPartitionEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.view.partition.save");
         final List<PartitionDto> partitionDtos = event.getPartitions();
         final MetacatRequestContext context = event.getRequestContext();
         final List<ElasticSearchDoc> docs = partitionDtos.stream()
@@ -226,6 +251,8 @@ public class MetacatEventHandlers {
      */
     @Subscribe
     public void metacatSaveTablePartitionPostEventHandler(final MetacatSaveTablePartitionPostEvent event) {
+        log.debug("Received SaveTablePartitionEvent {}", event);
+        CounterWrapper.incrementCounter("metacat.elasticsearch.events.table.partition.save");
         final List<PartitionDto> partitionDtos = event.getPartitions();
         final MetacatRequestContext context = event.getRequestContext();
         final List<ElasticSearchDoc> docs = partitionDtos.stream()
