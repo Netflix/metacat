8c70eb086413a19ec37f52d8ca1d967e2a9d865d
Tom Gianos <tgianos@users.noreply.github.com>
2017-06-07 10:13:40 -0700

    Optional Eureka registration (#139)

---
+++ metacat-main/src/main/java/com/netflix/metacat/main/configs/EurekaConfig.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  Copyright 2017 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.metacat.main.configs;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Conditional Configuration of the Eureka client.
+ *
+ * @author tgianos
+ */
+@ConditionalOnProperty(value = "eureka.client.enabled", havingValue = "true")
+@Configuration
+@EnableEurekaClient
+public class EurekaConfig {
+}
