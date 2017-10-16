Netflix/metacat

commit 31157675c607b49b32c3bc98ab76605f8dbeb110

Bypassing hive metastore to add/alter/drop partitions. (#191)
* Bypassing hive metastore to add/alter/drop partitions.

metacat-functional-tests/metacat-test-cluster/etc-metacat/catalog/embedded-fast-hive-metastore.properties
@@ -1,0 +1,78 @@
+metacat.thrift.port=12004
+connector.name=hive
+hive.use.embedded.metastore=true
+hive.use.embedded.fastservice=true
+hive.exec.compress.intermediate=true
+hive.exec.max.dynamic.partitions.pernode=10000
+hive.exec.max.dynamic.partitions=300000
+hive.exec.parallel=true
+hive.exec.reducers.max=200
+hive.exec.scratchdir=/tmp/metacat/scratch
+hive.hadoop.supports.splittable.combineinputformat=true
+hive.intermediate.compression.codec=org.apache.hadoop.io.compress.SnappyCodec
+hive.intermediate.compression.type=BLOCK
+hive.mapjoin.localtask.max.memory.usage=0.9
+hive.mapjoin.smalltable.filesize=100000000
+hive.mapred.mode=strict
+hive.optimize.s3.query=true
+hive.ppd.remove.duplicatefilters=false
+hive.security.authorization.createtable.owner.grants=ALL
+hive.security.authorization.enabled=true
+mapred.max.split.size=200000000
+webinterface.private.actions=true
+hive.metastore.warehouse.dir=file://tmp/hive/warehouse
+hive.metastore.checkForDefaultDb=true
+
+datanucleus.autoCreateSchema=true
+datanucleus.fixedDatastore=false
+datanucleus.autoCreateTables=true
+
+fs.s3.impl=org.apache.hadoop.fs.s3.S3FileSystem
+fs.s3n.impl=org.apache.hadoop.fs.s3native.NativeS3FileSystem
+fs.default.name=hdfs://10.155.133.15:9000
+hadoop.tmp.dir=/tmp/metacat/hadoop/tmp
+fs.s3.buffer.dir=/tmp/metacat/hadoop/s3,/tmp/metacat/hadoop/s3
+io.compression.codecs=org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzoCodec,com.hadoop.compression.lzo.LzopCodec,org.apache.hadoop.io.compress.BZip2Codec,org.apache.hadoop.io.compress.SnappyCodec
+fs.s3bfs.impl=org.apache.hadoop.fs.s3.S3FileSystem
+hadoop.metrics.defaultFile=/home/hadoop/conf/hadoopDefaultMetricsList
+hadoop.metrics.list=TotalLoad,CapacityTotalGB,UnderReplicatedBlocks,CapacityRemainingGB,PendingDeletionBlocks,PendingReplicationBlocks,CorruptBlocks,CapacityUsedGB,numLiveDataNodes,numDeadDataNodes,MissingBlocks
+io.compression.codec.lzo.class=com.hadoop.compression.lzo.LzoCodec
+ipc.client.connect.max.retries=50
+fs.s3n.endpoint=s3-external-1.amazonaws.com
+s3mper.disable=false
+s3mper.listing.recheck.count=15
+s3mper.listing.recheck.period=60000
+s3mper.metastore.retry=5
+s3mper.metastore.timeout=30000
+mapreduce.jobtracker.system.dir.permission=777
+hive.metastore.fs.handler.class=com.netflix.metacat.connector.hive.metastore.HiveMetaStoreFsImpl
+
+
+javax.jdo.option.name=embedded-fast-hive-metastore
+javax.jdo.option.username=metacat_user
+javax.jdo.option.url=jdbc:mysql://hive-metastore-db:3306/fasthive?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=latin1&autoReconnect=true&rewriteBatchedStatements=true
+javax.jdo.option.driverClassName=com.mysql.jdbc.Driver
+javax.jdo.option.password=metacat_user_password
+javax.jdo.option.jmxEnabled=true
+javax.jdo.option.maxActive=10
+javax.jdo.option.maxIdle=10
+javax.jdo.option.initialSize=2
+javax.jdo.option.minIdle=2
+javax.jdo.option.maxWait=60000
+javax.jdo.option.minEvictableIdleTimeMillis=180000
+javax.jdo.option.timeBetweenEvictionRunsMillis=10000
+javax.jdo.option.testOnBorrow=true
+javax.jdo.option.testWhileIdle=true
+javax.jdo.option.testOnReturn=false
+javax.jdo.option.removeAbandonedTimeout=1800
+javax.jdo.option.removeAbandoned=true
+javax.jdo.option.logAbandoned=true
+javax.jdo.option.validationQuery=SELECT 1
+javax.jdo.option.jdbcInterceptors=org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer;org.apache.tomcat.jdbc.pool.interceptor.SlowQueryReportJmx(threshold=30000)
+javax.jdo.option.defaultTransactionIsolation=READ_COMMITTED
+javax.jdo.option.defaultAutoCommit=false
+
+hive.allow-drop-table=true
+hive.allow-rename-table=true
+metacat.schema.blacklist=information_schema
+hive.use.embedded.sql.save.partitions=true

build.gradle
@@ -151,7 +151,7 @@ configure(javaProjects) {
         all*.exclude module: "jcl-over-slf4j"
         all*.exclude module: "log4j-over-slf4j"
         all*.exclude module: "slf4j-jdk14"
-        all*.exclude group: "ch.qos.logback", module: "logback-classic"
+        all*.exclude module: "slf4j-log4j12"
     }
 
     dependencies {

metacat-common-server/src/main/java/com/netflix/metacat/common/server/properties/Config.java
@@ -144,6 +144,20 @@ public interface Config {
     String getHivePartitionWhitelistPattern();
 
     /**
+     * Number of records to fetch in one call from Hive Metastore.
+     *
+     * @return fetch size
+     */
+    int getHiveMetastoreFetchSize();
+
+    /**
+     * Number of records to save in one call to Hive Metastore.
+     *
+     * @return batch size
+     */
+    int getHiveMetastoreBatchSize();
+
+    /**
      * Lookup service admin user name.
      *
      * @return user name

metacat-common-server/src/main/java/com/netflix/metacat/common/server/properties/DefaultConfigImpl.java
@@ -201,6 +201,22 @@ public class DefaultConfigImpl implements Config {
      * {@inheritDoc}
      */
     @Override
+    public int getHiveMetastoreFetchSize() {
+        return this.metacatProperties.getHive().getMetastore().getFetchSize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getHiveMetastoreBatchSize() {
+        return this.metacatProperties.getHive().getMetastore().getBatchSize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public String getLookupServiceUserAdmin() {
         return this.metacatProperties.getLookupService().getUserAdmin();
     }

metacat-common-server/src/main/java/com/netflix/metacat/common/server/properties/HiveProperties.java
@@ -44,6 +44,8 @@ public class HiveProperties {
 
         @NonNull
         private Partition partition = new Partition();
+        private int fetchSize = 2500;
+        private int batchSize = 2500;
 
         /**
          * Metastore partition related properties.

metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/HiveConnectorFactory.java
@@ -77,8 +77,6 @@ public class HiveConnectorFactory implements ConnectorFactory {
         properties.put("useEmbeddedClient", useLocalMetastore);
         propertySources.addFirst(new MapPropertySource("HIVE_CONNECTOR", properties));
         this.ctx.setEnvironment(standardEnvironment);
-        //TODO scan the package, which is not working
-//        this.ctx.scan(this.getClass().getPackage().getName());
         this.ctx.register(HiveConnectorFastServiceConfig.class);
         this.ctx.register(HiveConnectorClientConfig.class);
         this.ctx.register(HiveConnectorConfig.class);

commit edb7ff4d6632868f904e316b2d4d9c4325bac187

Fixed issue introduced in PR #191. Issue: Partition parameters were not getting stored.

metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/sql/DirectSqlSavePartition.java
@@ -135,6 +135,13 @@ public class DirectSqlSavePartition {
             for (int i = 0; i < partValues.size(); i++) {
                 partitionKeyValsValues.add(new Object[] {partId, partValues.get(i), i});
             }
+            // Partition parameters
+            final Map<String, String> parameters = partition.getMetadata();
+            if (parameters != null) {
+                parameters
+                    .forEach((key, value) -> partitionParamsValues.add(new Object[] {value, partId, key }));
+            }
+            partitionParamsValues.add(new Object[] {currentTimeInEpoch, partId, PARAM_LAST_DDL_TIME });
             if (storageInfo != null) {
                 serdesValues.add(new Object[]{null, storageInfo.getSerializationLib(), serdeId});
                 final Map<String, String> serdeInfoParameters = storageInfo.getSerdeInfoParameters();
@@ -144,13 +151,6 @@ public class DirectSqlSavePartition {
                 }
                 sdsValues.add(new Object[]{storageInfo.getOutputFormat(), false, tableSequenceIds.getCdId(),
                     false, serdeId, storageInfo.getUri(), storageInfo.getInputFormat(), 0, sdsId, });
-                final Map<String, String> parameters = storageInfo.getParameters();
-                if (parameters != null) {
-                    parameters
-                        .forEach((key, value) -> partitionParamsValues.add(new Object[] {value, partId, key }));
-                    partitionParamsValues.add(
-                        new Object[] {currentTimeInEpoch, partId, PARAM_LAST_DDL_TIME });
-                }
             }
             partitionNames.add(partitionName);
             currentIndex++;
@@ -226,6 +226,14 @@ public class DirectSqlSavePartition {
             final long partId = partitionHolder.getId();
             final long sdsId = partitionHolder.getSdId();
             final long serdeId = partitionHolder.getSerdeId();
+            // Partition parameters
+            final Map<String, String> parameters = partition.getMetadata();
+            if (parameters != null) {
+                parameters
+                    .forEach((key, value) -> partitionParamsValues.add(new Object[] {value, partId, key, value }));
+            }
+            partitionParamsValues.add(
+                new Object[] {currentTimeInEpoch, partId, PARAM_LAST_DDL_TIME, currentTimeInEpoch });
             if (storageInfo != null) {
                 serdesValues.add(new Object[]{null, storageInfo.getSerializationLib(), serdeId});
                 final Map<String, String> serdeInfoParameters = storageInfo.getSerdeInfoParameters();
@@ -235,13 +243,6 @@ public class DirectSqlSavePartition {
                 }
                 sdsValues.add(new Object[]{storageInfo.getOutputFormat(), false, false, storageInfo.getUri(),
                     storageInfo.getInputFormat(), sdsId, });
-                final Map<String, String> parameters = storageInfo.getParameters();
-                if (parameters != null) {
-                    parameters
-                        .forEach((key, value) -> partitionParamsValues.add(new Object[] {value, partId, key, value }));
-                    partitionParamsValues.add(
-                        new Object[] {currentTimeInEpoch, partId, PARAM_LAST_DDL_TIME, currentTimeInEpoch });
-                }
             }
             partitionNames.add(partition.getName().toString());
         }

metacat-functional-tests/metacat-test-cluster/etc-metacat/catalog/embedded-fast-hive-metastore.properties
@@ -50,7 +50,7 @@ hive.metastore.fs.handler.class=com.netflix.metacat.connector.hive.metastore.Hiv
 
 javax.jdo.option.name=embedded-fast-hive-metastore
 javax.jdo.option.username=metacat_user
-javax.jdo.option.url=jdbc:mysql://hive-metastore-db:3306/fasthive?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=latin1&autoReconnect=true&rewriteBatchedStatements=true
+javax.jdo.option.url=jdbc:mysql://hive-metastore-db:3306/fasthive?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=latin1&autoReconnect=true&sessionVariables=@@innodb_lock_wait_timeout=120&rewriteBatchedStatements=true
 javax.jdo.option.driverClassName=com.mysql.jdbc.Driver
 javax.jdo.option.password=metacat_user_password
 javax.jdo.option.jmxEnabled=true

metacat-functional-tests/metacat-test-cluster/etc-metacat/usermetadata.properties
@@ -15,7 +15,7 @@
 #
 #
 
-javax.jdo.option.url=jdbc:mysql://hive-metastore-db:3306/metacat?useUnicode=true&characterEncoding=latin1&autoReconnect=true&rewriteBatchedStatements=true
+javax.jdo.option.url=jdbc:mysql://hive-metastore-db:3306/metacat?useUnicode=true&characterEncoding=latin1&autoReconnect=true&sessionVariables=@@innodb_lock_wait_timeout=120&rewriteBatchedStatements=true
 javax.jdo.option.username=metacat_user
 javax.jdo.option.driverClassName=com.mysql.jdbc.Driver
 javax.jdo.option.password=metacat_user_password

metacat-functional-tests/src/functionalTest/groovy/com/netflix/metacat/MetacatSmokeSpec.groovy
@@ -469,11 +469,12 @@ class MetacatSmokeSpec extends Specification {
                 it.name.partitionName == partitionName
             } != null
             def partitionDetails = partitionApi.getPartitionsForRequest(catalogName, databaseName, tableName, null, null, null, null, true, new GetPartitionsRequestDto(filter: partitionName.replace('=', '="') + '"', includePartitionDetails: true))
-            assert partitionDetails != null && partitionDetails.size() == 1 && partitionDetails.find {
-                it.name.partitionName == partitionName
-            } != null && partitionDetails.size() == partitions.size() && partitionDetails.find {
-                it.name.partitionName == partitionName
-            }.getSerde().getSerdeInfoParameters().size() >= 1
+            def partitionDetail = partitionDetails.find { it.name.partitionName == partitionName }
+            assert partitionDetails != null && partitionDetails.size() == 1 && partitionDetail != null && partitionDetails.size() == partitions.size() && partitionDetail.getSerde().getSerdeInfoParameters().size() >= 1
+            if (catalogName != 's3-mysql-db') {
+                assert partitionDetail.getMetadata().size() >= 1
+                assert partitionDetail.getMetadata().get('functionalTest') == 'true'
+            }
             if (repeat) {
                 assert api.getTable(catalogName, databaseName, tableName, false, true, false).getDefinitionMetadata().get('savePartitions') != null
                 assert partitions.get(0).dataUri == uri + 0

metacat-testdata-provider/src/main/groovy/com/netflix/metacat/testdata/provider/PigDataDtoProvider.groovy
@@ -125,6 +125,7 @@ class PigDataDtoProvider {
                         serializationLib: 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe',
                         uri: uri
                 ),
+                metadata: ['functionalTest': 'true'],
                 definitionMetadata: getDefinitionMetadata('test'),
                 dataMetadata: metacatJson.parseJsonObject('{"metrics": {}}')
         )