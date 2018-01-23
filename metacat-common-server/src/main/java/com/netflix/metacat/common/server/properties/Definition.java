2207aca3ee23d03a8a7e7bcf20cd653cf65f4a7b
amajumdar <ajoymajumdar@hotmail.com>
2018-01-22 22:44:52 -0800

    Added support to not delete definition metadata when dropping a table. Action to delete definition metadata now depends on a configuration. When a whitelist is defined, definition metadata for those tables should be deleted.

--- metacat-common-server/src/main/java/com/netflix/metacat/common/server/properties/Definition.java
+++ metacat-common-server/src/main/java/com/netflix/metacat/common/server/properties/Definition.java
@@ -1,18 +1,18 @@
 /*
  *
- *  Copyright 2017 Netflix, Inc.
+ * Copyright 2018 Netflix, Inc.
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  *
  */
 package com.netflix.metacat.common.server.properties;
@@ -27,7 +27,7 @@ import java.util.Set;
  * Definition metadata related properties.
  *
  * @author amajumdar
- * @since 1.1.1
+ * @since 1.2.0
  */
 @lombok.Data
 public class Definition {
@@ -39,7 +39,7 @@ public class Definition {
      * Metadata related properties.
      *
      * @author amajumdar
-     * @since 1.1.1
+     * @since 1.2.0
      */
     @lombok.Data
     public static class Metadata {
@@ -51,7 +51,7 @@ public class Definition {
          * Delete related properties.
          *
          * @author amajumdar
-         * @since 1.1.1
+         * @since 1.2.0
          */
         @lombok.Data
         public static class Delete {
