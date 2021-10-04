7e70ed6a91b73ec79616a50e9b34241ac9c9ebb7
Abhi Shah <abshah@netflix.com>
2021-10-04 16:41:39 -0700

    Added polaris connector project.

---
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/PolarisPersistenceConfig.java
@@ -0,0 +1,56 @@
+package com.netflix.metacat.connector.polaris;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * The Polaris Store Persistence config.
+ *
+ */
+@Configuration
+@EntityScan("com.netflix.metacat.connector.polaris")
+@EnableJpaRepositories("com.netflix.metacat.connector.polaris")
+public class PolarisPersistenceConfig {
+
+  /**
+   * Primary datasource. Since connectors can have data sources configured, polaris store JPA needs to be
+   * explicitly configured.
+   *
+   * @param dataSourceProperties datasource properties
+   * @return Datasource
+   */
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource.hikari")
+  public DataSource dataSource(final DataSourceProperties dataSourceProperties) {
+    return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+
+  /**
+   * Datasource properties.
+   * @return DataSourceProperties
+   */
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource")
+  public DataSourceProperties dataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  /**
+   * Get an implementation of {@link PolarisConnector}.
+   * @param repo - PolarisDatabaseRepository
+   * @return PolarisConnector
+   */
+  @Bean
+  public PolarisConnector polarisConnector(final PolarisDatabaseRepository repo) {
+    return new PolarisConnector(repo);
+  }
+}
