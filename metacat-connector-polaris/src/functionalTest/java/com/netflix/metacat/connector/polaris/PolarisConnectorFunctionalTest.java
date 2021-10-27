a4c005c1dee684d7db26edeb3ae8dbf4af00eca3
Abhi Shah <abshah@netflix.com>
2021-10-27 09:53:10 -0700

    added functional test for polaris

---
+++ metacat-connector-polaris/src/functionalTest/java/com/netflix/metacat/connector/polaris/PolarisConnectorFunctionalTest.java
@@ -0,0 +1,19 @@
+package com.netflix.metacat.connector.polaris;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Test persistence operations on Database objects.
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {PolarisPersistenceConfig.class})
+@ActiveProfiles(profiles = {"polaris_functional_test"})
+@AutoConfigureDataJpa
+public class PolarisConnectorFunctionalTest extends PolarisConnectorTest {
+
+}
---
+++ metacat-connector-polaris/src/functionalTest/resources/schema.sql
@@ -0,0 +1,17 @@
+drop table IF EXISTS TBLS;
+drop table IF EXISTS DBS;
+
+create table DBS (
+  version bigint not null,
+  id uuid default gen_random_uuid() not null primary key,
+  name varchar(255) not null unique
+);
+
+create table TBLS (
+  version bigint not null,
+  id uuid default gen_random_uuid() not null primary key,
+  db_name varchar(255) not null,
+  tbl_name varchar(255) not null,
+  constraint uniq_name unique(db_name, tbl_name),
+  foreign key (db_name) references DBS(name)
+);
