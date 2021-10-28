5739cab96c22d80872d7892aff7c4bac274477f3
Abhi Shah <abshah@netflix.com>
2021-10-28 15:04:45 -0700

    Skeletal metacat-metadata module.

---
+++ metacat-metadata/src/functionalTest/scripts/start_docker_containers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo PATH is $PATH
+echo DOCKER env is $(env | grep DOCKER)
+
+COMPOSE_FILE=$1
+
+docker-compose --file ${COMPOSE_FILE} up crdb-barrier
+if [ $? -ne 0 ]; then
+    echo "Unable to start crdb-barrier container"
+    exit 9
+fi
+
+docker-compose --file ${COMPOSE_FILE} up -d crdb
+if [ $? -ne 0 ]; then
+    echo "Unable to start crdb container"
+    exit 10
+fi
