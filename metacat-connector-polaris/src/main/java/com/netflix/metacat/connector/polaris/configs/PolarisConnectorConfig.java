c1c34d30e5599c61c52bdc5ea29570075a092fbe
jtian <86805957+rsyncv@users.noreply.github.com>
2021-11-10 10:52:22 -0800

    Integrate Iceberg client and get table implementation

--- metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/configs/PolarisConnectorConfig.java
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/configs/PolarisConnectorConfig.java
@@ -1,19 +1,25 @@
 package com.netflix.metacat.connector.polaris.configs;
 
 import com.netflix.metacat.common.server.connectors.ConnectorContext;
+import com.netflix.metacat.common.server.util.ThreadServiceManager;
 import com.netflix.metacat.connector.hive.converters.HiveConnectorInfoConverter;
+import com.netflix.metacat.connector.hive.iceberg.IcebergTableCriteria;
+import com.netflix.metacat.connector.hive.iceberg.IcebergTableCriteriaImpl;
+import com.netflix.metacat.connector.hive.iceberg.IcebergTableHandler;
+import com.netflix.metacat.connector.hive.iceberg.IcebergTableOpWrapper;
+import com.netflix.metacat.connector.hive.iceberg.IcebergTableOpsProxy;
 import com.netflix.metacat.connector.polaris.PolarisConnectorDatabaseService;
-import com.netflix.metacat.connector.polaris.store.PolarisStoreConnector;
 import com.netflix.metacat.connector.polaris.PolarisConnectorTableService;
+import com.netflix.metacat.connector.polaris.store.PolarisStoreConnector;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 /**
- * config for polaris connector.
+ * Config for polaris connector.
  */
 public class PolarisConnectorConfig {
     /**
-     * create polaris connector database service.
+     * Create polaris connector database service.
      *
      * @param polarisConnector polaris connector
      * @return PolarisConnectorDatabaseService
@@ -27,11 +33,12 @@ public class PolarisConnectorConfig {
     }
 
     /**
-     * create polaris connector table service.
+     * Create polaris connector table service.
      *
      * @param polarisConnector          polaris connector
      * @param connectorConverter        connector converter
      * @param connectorDatabaseService  polaris database service
+     * @param icebergTableHandler       iceberg table handler
      * @param connectorContext          connector context
      * @return PolarisConnectorTableService
      */
@@ -41,6 +48,7 @@ public class PolarisConnectorConfig {
         final PolarisStoreConnector polarisConnector,
         final HiveConnectorInfoConverter connectorConverter,
         final PolarisConnectorDatabaseService connectorDatabaseService,
+        final IcebergTableHandler icebergTableHandler,
         final ConnectorContext connectorContext
     ) {
         return new PolarisConnectorTableService(
@@ -48,7 +56,70 @@ public class PolarisConnectorConfig {
             connectorContext.getCatalogName(),
             connectorDatabaseService,
             connectorConverter,
+            icebergTableHandler,
             connectorContext
         );
+    }
+
+    /**
+     * Create iceberg table handler.
+     * @param connectorContext      server context
+     * @param icebergTableCriteria  iceberg table criteria
+     * @param icebergTableOpWrapper iceberg table operation
+     * @param icebergTableOpsProxy  IcebergTableOps proxy
+     * @return IcebergTableHandler
+     */
+    @Bean
+    public IcebergTableHandler icebergTableHandler(final ConnectorContext connectorContext,
+                                                   final IcebergTableCriteria icebergTableCriteria,
+                                                   final IcebergTableOpWrapper icebergTableOpWrapper,
+                                                   final IcebergTableOpsProxy icebergTableOpsProxy) {
+        return new IcebergTableHandler(
+            connectorContext,
+            icebergTableCriteria,
+            icebergTableOpWrapper,
+            icebergTableOpsProxy);
+    }
+
+    /**
+     *
+     * Create iceberg table criteria.
+     * @param connectorContext server context
+     * @return IcebergTableCriteria
+     */
+    @Bean
+    public IcebergTableCriteria icebergTableCriteria(final ConnectorContext connectorContext) {
+        return new IcebergTableCriteriaImpl(connectorContext);
+    }
+
+    /**
+     * Create iceberg table operation wrapper.
+     * @param connectorContext     server context
+     * @param threadServiceManager executor service
+     * @return IcebergTableOpWrapper
+     */
+    @Bean
+    public IcebergTableOpWrapper icebergTableOpWrapper(final ConnectorContext connectorContext,
+                                                       final ThreadServiceManager threadServiceManager) {
+        return new IcebergTableOpWrapper(connectorContext, threadServiceManager);
+    }
+
+    /**
+     * Create thread service manager.
+     * @param connectorContext connector config
+     * @return ThreadServiceManager
+     */
+    @Bean
+    public ThreadServiceManager threadServiceManager(final ConnectorContext connectorContext) {
+        return new ThreadServiceManager(connectorContext.getRegistry(), connectorContext.getConfig());
+    }
+
+    /**
+     * Create IcebergTableOps proxy.
+     * @return IcebergTableOpsProxy
+     */
+    @Bean
+    public IcebergTableOpsProxy icebergTableOps() {
+        return new IcebergTableOpsProxy();
     }
 }
