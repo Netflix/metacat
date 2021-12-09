3393108fd2d984c1dacfe0307793673fd738c088
jtian <86805957+rsyncv@users.noreply.github.com>
2021-12-08 16:21:27 -0800

    Polaris table update api with tests

--- metacat-connector-polaris/src/functionalTest/resources/schema.sql
+++ metacat-connector-polaris/src/functionalTest/resources/schema.sql
@@ -14,5 +14,5 @@ create table TBLS (
   tbl_name varchar(255) not null,
   metadata_location varchar(1024),
   constraint uniq_name unique(db_name, tbl_name),
-  foreign key (db_name) references DBS(name)
+  foreign key (db_name) references DBS(name) ON DELETE CASCADE ON UPDATE CASCADE
 );
