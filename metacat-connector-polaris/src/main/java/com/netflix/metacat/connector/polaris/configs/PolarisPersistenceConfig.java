f2f5b10d784a7c38c2bb89438bcdf7ed086645e7
rveeramacheneni <rveeramacheneni@netflix.com>
2022-01-14 09:26:13 -0800

    Add explicit enable transaction mgmt annotation

--- metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/configs/PolarisPersistenceConfig.java
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/configs/PolarisPersistenceConfig.java
@@ -12,11 +12,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -27,8 +29,10 @@ import javax.sql.DataSource;
 @Configuration
 @EntityScan("com.netflix.metacat.connector.polaris.store.entities")
 @EnableJpaRepositories("com.netflix.metacat.connector.polaris.store.repos")
+@EnableTransactionManagement(proxyTargetClass = true)
 @ImportAutoConfiguration({DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+        DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
+        TransactionAutoConfiguration.class})
 public class PolarisPersistenceConfig {
 
   /**
