73a37fe77b94c66189383da9489ef7fe49c674b4
jtian <86805957+insyncoss@users.noreply.github.com>
2024-05-06 16:37:13 -0700

    Publish new name on table rename event

--- metacat-common/src/main/java/com/netflix/metacat/common/dto/notifications/sns/messages/RenameTableMessage.java
+++ metacat-common/src/main/java/com/netflix/metacat/common/dto/notifications/sns/messages/RenameTableMessage.java
@@ -26,6 +26,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.annotation.Nullable;
+
 /**
  * A message sent when a table is renamed.
  *
@@ -35,6 +37,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class RenameTableMessage extends UpdateOrRenameTableMessageBase {
+    private final String newName;
 
     /**
      * Create a new RenameTableMessage.
@@ -42,7 +45,8 @@ public class RenameTableMessage extends UpdateOrRenameTableMessageBase {
      * @param id        The unique id of the message
      * @param timestamp The number of milliseconds since epoch that this message occurred
      * @param requestId The id of the API request that generated this and possibly other messages. Used for grouping
-     * @param name      The qualified name of the resource that this notification is being generated for
+     * @param name      The previous qualified name of the resource that this notification is being generated for
+     * @param newName   The new qualified name of the resource that this notification is being generated for
      * @param payload   The payload of the notification
      */
     @JsonCreator
@@ -51,8 +55,10 @@ public class RenameTableMessage extends UpdateOrRenameTableMessageBase {
         @JsonProperty("timestamp") final long timestamp,
         @JsonProperty("requestId") final String requestId,
         @JsonProperty("name") final String name,
-        @JsonProperty("payload") final UpdatePayload<TableDto> payload
+        @JsonProperty("newName") final String newName,
+        @Nullable @JsonProperty("payload") final UpdatePayload<TableDto> payload
     ) {
         super(id, timestamp, requestId, name, payload, SNSMessageType.TABLE_RENAME);
+        this.newName = newName;
     }
 }
