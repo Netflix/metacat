445a8be3cbaee01d2f8b0715428cdaae943a1f1b
Yingjian Wu <yingjianw@netflix.com>
2024-11-04 22:40:55 -0800

    asda

--- metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/client/embedded/HivePersistenceManagerFactory.java
+++ metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/client/embedded/HivePersistenceManagerFactory.java
@@ -20,21 +20,22 @@ import com.google.common.collect.Maps;
 import com.netflix.metacat.common.server.util.DataSourceManager;
 import com.netflix.metacat.connector.hive.util.HiveConfigConstants;
 import lombok.extern.slf4j.Slf4j;
-import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-import javax.jdo.PersistenceManagerFactory;
 import javax.sql.DataSource;
 import java.util.Map;
 
 /**
  * HivePersistenceManagerFactory.
  *
- * @author zhenl
+ * Manages Hibernate SessionFactory instances based on application configurations.
+ *
  * @since 1.0.0
  */
 @Slf4j
 public final class HivePersistenceManagerFactory {
-    private static Map<String, PersistenceManagerFactory> factories = Maps.newConcurrentMap();
+    private static Map<String, SessionFactory> factories = Maps.newConcurrentMap();
 
     private HivePersistenceManagerFactory() {
     }
@@ -43,53 +44,34 @@ public final class HivePersistenceManagerFactory {
      * getPersistenceManagerFactory.
      *
      * @param props props
-     * @return PersistenceManagerFactory
+     * @return SessionFactory
      */
-    public static PersistenceManagerFactory getPersistenceManagerFactory(final Map props) {
+    public static SessionFactory getPersistenceManagerFactory(final Map props) {
         final String name = String.valueOf(props.get(HiveConfigConstants.JAVAX_JDO_OPTION_NAME));
-        PersistenceManagerFactory result = factories.get(name);
+        SessionFactory result = factories.get(name);
         if (result == null) {
-            result = getpersistencemanagerfactory(props);
+            result = createSessionFactory(name, props);
         }
         return result;
     }
 
-    private static synchronized PersistenceManagerFactory getpersistencemanagerfactory(final Map props) {
-        final String name = String.valueOf(props.get(HiveConfigConstants.JAVAX_JDO_OPTION_NAME));
-        PersistenceManagerFactory result = factories.get(name);
+    private static synchronized SessionFactory createSessionFactory(final String name, final Map props) {
+        SessionFactory result = factories.get(name);
         if (result == null) {
             final DataSource dataSource = DataSourceManager.get().get(name);
-            final Map<String, Object> properties = Maps.newHashMap();
-            properties.put(HiveConfigConstants.DATANUCLEUS_FIXEDDATASTORE,
-                props.getOrDefault(HiveConfigConstants.DATANUCLEUS_FIXEDDATASTORE, true));
-            properties.put(HiveConfigConstants.DATANUCLEUS_AUTOCREATESCHEMA,
-                props.getOrDefault(HiveConfigConstants.DATANUCLEUS_AUTOCREATESCHEMA, false));
-            properties.put(HiveConfigConstants.DATANUCLEUS_RDBMS_CHECKEXISTTABLESORVIEWS,
-                props.getOrDefault(HiveConfigConstants.DATANUCLEUS_RDBMS_CHECKEXISTTABLESORVIEWS, false));
-            properties.put(HiveConfigConstants.DATANUCLEUS_RDBMS_INITIALIZECOULUMNINFO,
-                props.getOrDefault(HiveConfigConstants.DATANUCLEUS_RDBMS_INITIALIZECOULUMNINFO, "None"));
-            properties.put(HiveConfigConstants.DATANUCLEUS_IDENTIFIERFACTORY,
-                HiveConfigConstants.DATANUCLEUS_DATANUCLEU1);
-            properties.put(HiveConfigConstants.DATANUCLEUS_CONNECTIONFACTORY, dataSource);
-            properties.put(HiveConfigConstants.DATANUCLEUS_RDBMS_USELEGACYNATIVEVALUESTRATEGY, true);
-            properties.put(HiveConfigConstants.DATANUCLEUS_TRANSACTIONISOLATION,
-                HiveConfigConstants.DATANUCLEUS_READCOMMITTED);
-            properties.put(HiveConfigConstants.DATANUCLEUS_VALIDATETABLE, false);
-            properties.put(HiveConfigConstants.DATANUCLEUS_VALIDATECONSTRAINTS, false);
-            properties.put(HiveConfigConstants.DATANUCLEUS_VALIDATECOLUMNS, false);
-            properties.put(HiveConfigConstants.DATANUCLEUS_CACHE_LEVEL2, false);
-            properties.put(HiveConfigConstants.DATANUCLEUS_CACHE_LEVEL2_TYPE, "none");
-            properties.put(HiveConfigConstants.DATANUCLEUS_PERSISTENCYBYREACHATCOMMIT, false);
-            properties.put(HiveConfigConstants.DATANUCLEUS_AUTOSTARTMECHANISMMODE, "Checked");
-            properties.put(HiveConfigConstants.DATANUCLEUS_DETACHALLONCOMMIT, true);
-            properties.put(HiveConfigConstants.DATANUCLEUS_DETACHALLONROLLBACK, true);
-            properties.put(HiveConfigConstants.JAVAX_JDO_DATASTORETIMEOUT,
-                props.get(HiveConfigConstants.JAVAX_JDO_DATASTORETIMEOUT));
-            properties.put(HiveConfigConstants.JAVAX_JDO_DATASTOREREADTIMEOUT,
-                props.get(HiveConfigConstants.JAVAX_JDO_DATASTOREREADTIMEOUT));
-            properties.put(HiveConfigConstants.JAVAX_JDO_DATASTOREWRITETIMEOUT,
-                props.get(HiveConfigConstants.JAVAX_JDO_DATASTOREWRITETIMEOUT));
-            result = JDOPersistenceManagerFactory.getPersistenceManagerFactory(properties);
+
+            // Configure Hibernate SessionFactory
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.datasource", dataSource.toString());
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+            configuration.setProperty(HiveConfigConstants.HIBERNATE_HBM2DDL_AUTO, String.valueOf(props.getOrDefault(HiveConfigConstants.HIBERNATE_HBM2DDL_AUTO, "update")));
+            configuration.setProperty(HiveConfigConstants.HIBERNATE_JDBC_TIMEOUT, String.valueOf(props.getOrDefault(HiveConfigConstants.HIBERNATE_JDBC_TIMEOUT, "30")));
+            configuration.setProperty(HiveConfigConstants.HIBERNATE_CONNECTION_ISOLATION, String.valueOf(props.getOrDefault(HiveConfigConstants.HIBERNATE_CONNECTION_ISOLATION, "2"))); // TRANSACTION_READ_COMMITTED
+            configuration.setProperty(HiveConfigConstants.HIBERNATE_CACHE_USE_SECOND_LEVEL_CACHE, String.valueOf(props.getOrDefault(HiveConfigConstants.HIBERNATE_CACHE_USE_SECOND_LEVEL_CACHE, "false")));
+            configuration.setProperty(HiveConfigConstants.HIBERNATE_CACHE_REGION_FACTORY_CLASS, String.valueOf(props.getOrDefault(HiveConfigConstants.HIBERNATE_CACHE_REGION_FACTORY_CLASS, "org.hibernate.cache.internal.NoCachingRegionFactory")));
+
+
+            result = configuration.buildSessionFactory();
             factories.put(name, result);
         }
         return result;
--- metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/util/HiveConfigConstants.java
+++ metacat-connector-hive/src/main/java/com/netflix/metacat/connector/hive/util/HiveConfigConstants.java
@@ -1,25 +1,10 @@
-/*
- *  Copyright 2017 Netflix, Inc.
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
-
 package com.netflix.metacat.connector.hive.util;
 
 /**
  * HiveConfigConstants.
  *
- * @author zhenl
+ * Updated for use with Hibernate.
+ *
  * @since 1.0.0
  */
 public final class HiveConfigConstants {
@@ -42,28 +27,34 @@ public final class HiveConfigConstants {
      * ALLOW_RENAME_TABLE.
      */
     public static final String ALLOW_RENAME_TABLE = "hive.allow-rename-table";
+
     /**
      * USE_FASTPARTITION_SERVICE.
      */
     public static final String USE_FASTHIVE_SERVICE = "hive.use.embedded.fastservice";
+
     /**
      * ENABLE_AUDIT_PROCESSING.
      */
     public static final String ENABLE_AUDIT_PROCESSING = "hive.use.embedded.fastservice.auditEnabled";
+
     /**
      * GET_PARTITION_DETAILS_TIMEOUT.
      */
     public static final String GET_PARTITION_DETAILS_TIMEOUT = "hive.use.embedded.GetPartitionDetailsTimeout";
+
     /**
      * GET_ICEBERG_PARTITIONS_TIMEOUT.
      */
     public static final String GET_ICEBERG_PARTITIONS_TIMEOUT = "hive.iceberg.GetIcebergPartitionsTimeout";
+
     /**
      * USE_FAST_DELETION.
      */
     public static final String USE_FAST_DELETION = "hive.use.embedded.sql.delete.partitions";
+
     /**
-     * USE_FASTPARTITION_SERVICE.
+     * THREAD_POOL_SIZE.
      */
     public static final String THREAD_POOL_SIZE = "hive.thread.pool.size";
 
@@ -78,122 +69,40 @@ public final class HiveConfigConstants {
     public static final String JAVAX_JDO_OPTION_NAME = "javax.jdo.option.name";
 
     /**
-     * JAVAX_JDO_DATASTORETIMEOUT.
+     * Hibernate equivalent for datastore timeout.
      */
-    public static final String JAVAX_JDO_DATASTORETIMEOUT = "javax.jdo.option.DatastoreTimeout";
+    public static final String HIBERNATE_JDBC_TIMEOUT = "hibernate.jdbc.timeout";
+
     /**
-     * JAVAX_JDO_DATASTOREREADTIMEOUT.
+     * Hibernate equivalent for connection isolation level.
      */
-    public static final String JAVAX_JDO_DATASTOREREADTIMEOUT = "javax.jdo.option.DatastoreReadTimeoutMillis";
+    public static final String HIBERNATE_CONNECTION_ISOLATION = "hibernate.connection.isolation";
+
     /**
-     * JAVAX_JDO_DATASTOREWRITETIMEOUT.
+     * Hibernate caching configuration.
      */
-    public static final String JAVAX_JDO_DATASTOREWRITETIMEOUT = "javax.jdo.option.DatastoreWriteTimeoutMillis";
+    public static final String HIBERNATE_CACHE_USE_SECOND_LEVEL_CACHE = "hibernate.cache.use_second_level_cache";
+    public static final String HIBERNATE_CACHE_REGION_FACTORY_CLASS = "hibernate.cache.region.factory_class";
+
     /**
-     * JAVAX_JDO_PERSISTENCEMANAGER_FACTORY_CLASS.
+     * Hibernate auto schema update.
      */
-    public static final String JAVAX_JDO_PERSISTENCEMANAGER_FACTORY_CLASS = "javax.jdo.PersistenceManagerFactoryClass";
-    /**
-     * JAVAX_JDO_PERSISTENCEMANAGER_FACTORY.
-     */
-    public static final String JAVAX_JDO_PERSISTENCEMANAGER_FACTORY
-            = "com.netflix.metacat.connector.hive.client.embedded.HivePersistenceManagerFactory";
+    public static final String HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+
     /**
      * HIVE_METASTORE_DS_RETRY.
      */
     public static final String HIVE_METASTORE_DS_RETRY = "hive.metastore.ds.retry.attempts";
+
     /**
      * HIVE_HMSHANDLER_RETRY.
      */
     public static final String HIVE_HMSHANDLER_RETRY = "hive.hmshandler.retry.attempts";
+
     /**
      * HIVE_STATS_AUTOGATHER.
      */
     public static final String HIVE_STATS_AUTOGATHER = "hive.stats.autogather";
-
-    /**
-     * DATANUCLEUS_AUTOSTARTMECHANISMMODE.
-     */
-    public static final String DATANUCLEUS_AUTOSTARTMECHANISMMODE = "datanucleus.autoStartMechanismMode";
-    /**
-     * DATANUCLEUS_DETACHALLONCOMMIT.
-     */
-    public static final String DATANUCLEUS_DETACHALLONCOMMIT = "datanucleus.detachAllOnCommit";
-    /**
-     * DATANUCLEUS_DETACHALLONROLLBACK.
-     */
-    public static final String DATANUCLEUS_DETACHALLONROLLBACK = "datanucleus.detachAllOnRollback";
-
-    /**
-     * DATANUCLEUS_PERSISTENCYBYREACHATCOMMIT.
-     */
-    public static final String DATANUCLEUS_PERSISTENCYBYREACHATCOMMIT = "datanucleus.persistenceByReachabilityAtCommit";
-
-    /**
-     * DATANUCLEUS_CACHE_LEVEL2_TYPE.
-     */
-    public static final String DATANUCLEUS_CACHE_LEVEL2_TYPE = "datanucleus.cache.level2.type";
-    /**
-     * DATANUCLEUS_CACHE_LEVEL2.
-     */
-    public static final String DATANUCLEUS_CACHE_LEVEL2 = "datanucleus.cache.level2";
-    /**
-     * DATANUCLEUS_VALIDATECOLUMNS.
-     */
-    public static final String DATANUCLEUS_VALIDATECOLUMNS = "datanucleus.validateColumns";
-    /**
-     * DATANUCLEUS_VALIDATECONSTRAINTS.
-     */
-    public static final String DATANUCLEUS_VALIDATECONSTRAINTS = "datanucleus.validateConstraints";
-    /**
-     * DATANUCLEUS_VALIDATETABLE.
-     */
-    public static final String DATANUCLEUS_VALIDATETABLE = "datanucleus.validateTables";
-    /**
-     * DATANUCLEUS_TRANSACTIONISOLATION.
-     */
-    public static final String DATANUCLEUS_TRANSACTIONISOLATION = "datanucleus.transactionIsolation";
-    /**
-     * DATANUCLEUS_READCOMMITTED.
-     */
-    public static final String DATANUCLEUS_READCOMMITTED = "read-committed";
-    /**
-     * DATANUCLEUS_FIXEDDATASTORE.
-     */
-    public static final String DATANUCLEUS_FIXEDDATASTORE = "datanucleus.fixedDatastore";
-
-    /**
-     * DATANUCLEUS_AUTOCREATESCHEMA.
-     */
-    public static final String DATANUCLEUS_AUTOCREATESCHEMA = "datanucleus.autoCreateSchema";
-    /**
-     * DATANUCLEUS_RDBMS_CHECKEXISTTABLESORVIEWS.
-     */
-    public static final String DATANUCLEUS_RDBMS_CHECKEXISTTABLESORVIEWS = "datanucleus.rdbms.CheckExistTablesOrViews";
-    /**
-     * DATANUCLEUS_RDBMS_INITIALIZECOULUMNINFO.
-     */
-    public static final String DATANUCLEUS_RDBMS_INITIALIZECOULUMNINFO = "datanucleus.rdbms.initializeColumnInfo";
-    /**
-     * DATANUCLEUS_IDENTIFIERFACTORY.
-     */
-    public static final String DATANUCLEUS_IDENTIFIERFACTORY = "datanucleus.identifierFactory";
-
-    /**
-     * DATANUCLEUS_DATANUCLEU1.
-     */
-    public static final String DATANUCLEUS_DATANUCLEU1 = "datanucleus1";
-
-    /**
-     * DATANUCLEUS_CONNECTIONFACTORY.
-     */
-    public static final String DATANUCLEUS_CONNECTIONFACTORY = "datanucleus.ConnectionFactory";
-
-    /**
-     * DATANUCLEUS_RDBMS_USELEGACYNATIVEVALUESTRATEGY.
-     */
-    public static final String DATANUCLEUS_RDBMS_USELEGACYNATIVEVALUESTRATEGY
-            = "datanucleus.rdbms.useLegacyNativeValueStrategy";
 
     /**
      * HIVE_HMSHANDLER_NAME.
