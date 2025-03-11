4f761439a3e934ecb3599c3f369b5e189307f816
Giorgio Trettenero <gtret@netflix.com>
2025-03-11 14:50:20 -0700

    add smoke test and delete from db

--- metacat-main/src/main/java/com/netflix/metacat/main/services/impl/TableServiceImpl.java
+++ metacat-main/src/main/java/com/netflix/metacat/main/services/impl/TableServiceImpl.java
@@ -435,7 +435,10 @@ public class TableServiceImpl implements TableService {
                 if (tableDto.getDefinitionMetadata() != null
                     && tableDto.getDefinitionMetadata().has(MetacatUtils.MIGRATED_DATA_LOCATION)) {
                     tableDto.getDefinitionMetadata().remove(MetacatUtils.MIGRATED_DATA_LOCATION);
-                    userMetadataService.saveMetadata(metacatRequestContext.getUserName(), tableDto, true);
+                    userMetadataService.saveDefinitionMetadata(tableDto.getName(),
+                        metacatRequestContext.getUserName(),
+                        Optional.of(tableDto.getDefinitionMetadata()),
+                            true);
                 }
                 userMetadataService.softDeleteDataMetadata(metacatRequestContext.getUserName(),
                     Lists.newArrayList(tableDto.getDataUri()));
