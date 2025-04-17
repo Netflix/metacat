eb449975a110fea072557e7b2dcdc9a4c733e853
Yingjian Wu <yingjianw@netflix.com>
2025-04-17 10:13:08 -0700

    make vtts transactional
    address style
    
    fix
    
    failureMessage
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    Adas
    
    wip
    
    wip
    
    fix

---
+++ metacat-common-server/src/main/java/com/netflix/metacat/common/server/usermetadata/MetadataPreMergeInterceptorImpl.java
@@ -0,0 +1,10 @@
+package com.netflix.metacat.common.server.usermetadata;
+
+/**
+ * MetadataPreMergeInterceptorImpl.
+ *
+ * @author yingjianw
+ * @since 1.2.0
+ */
+public class MetadataPreMergeInterceptorImpl implements MetadataPreMergeInterceptor {
+}
--- metacat-metadata-mysql/src/main/java/com/netflix/metacat/metadata/mysql/MySqlUserMetadataConfig.java
+++ metacat-metadata-mysql/src/main/java/com/netflix/metacat/metadata/mysql/MySqlUserMetadataConfig.java
@@ -17,6 +17,10 @@ import com.netflix.metacat.common.json.MetacatJson;
 import com.netflix.metacat.common.server.converter.ConverterUtil;
 import com.netflix.metacat.common.server.properties.Config;
 import com.netflix.metacat.common.server.properties.MetacatProperties;
+import com.netflix.metacat.common.server.usermetadata.MetadataPreMergeInterceptor;
+import com.netflix.metacat.common.server.usermetadata.MetadataPreMergeInterceptorImpl;
+import com.netflix.metacat.common.server.usermetadata.MetadataSqlInterceptor;
+import com.netflix.metacat.common.server.usermetadata.MetadataSqlInterceptorImpl;
 import com.netflix.metacat.common.server.usermetadata.UserMetadataService;
 import com.netflix.metacat.common.server.usermetadata.LookupService;
 import com.netflix.metacat.common.server.usermetadata.TagService;
@@ -55,12 +59,36 @@ public class MySqlUserMetadataConfig {
     }
 
     /**
+     * MetadataSQLInterceptor layer.
+     * @return MetadataSQLInterceptor
+     */
+    @Bean
+    @ConditionalOnMissingBean(MetadataSqlInterceptor.class)
+    public MetadataSqlInterceptor metadataSqlInterceptor(
+    ) {
+        return new MetadataSqlInterceptorImpl();
+    }
+
+    /**
+     * MetadataSQLInterceptor layer.
+     * @return MetadataSQLInterceptor
+     */
+    @Bean
+    @ConditionalOnMissingBean(MetadataPreMergeInterceptor.class)
+    public MetadataPreMergeInterceptor metadataPreMergeInterceptor(
+    ) {
+        return new MetadataPreMergeInterceptorImpl();
+    }
+
+    /**
      * User Metadata service.
      *
      * @param jdbcTemplate JDBC template
      * @param config       System config to use
      * @param metacatJson  Json Utilities to use
      * @param metadataInterceptor  business metadata manager
+     * @param metadataPreMergeInterceptor metadataPreMergeInterceptor
+     * @param metadataSqlInterceptor metadataSQLInterceptor
      * @return User metadata service based on MySql
      */
     @Bean
@@ -68,9 +96,18 @@ public class MySqlUserMetadataConfig {
         @Qualifier("metadataJdbcTemplate") final JdbcTemplate jdbcTemplate,
         final Config config,
         final MetacatJson metacatJson,
-        final MetadataInterceptor metadataInterceptor
-    ) {
-        return new MysqlUserMetadataService(jdbcTemplate, metacatJson, config, metadataInterceptor);
+        final MetadataInterceptor metadataInterceptor,
+        final MetadataPreMergeInterceptor metadataPreMergeInterceptor,
+        final MetadataSqlInterceptor metadataSqlInterceptor
+        ) {
+        return new MysqlUserMetadataService(
+            jdbcTemplate,
+            metacatJson,
+            config,
+            metadataInterceptor,
+            metadataPreMergeInterceptor,
+            metadataSqlInterceptor
+        );
     }
 
 
