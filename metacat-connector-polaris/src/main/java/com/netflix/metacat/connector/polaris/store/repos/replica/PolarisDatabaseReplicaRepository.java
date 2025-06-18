7d3eaa5d728bbbc5e07141200e361d88bc920d31
Yingjian Wu <yingjianw@netflix.com>
2025-06-18 08:35:35 -0700

    asda

--- metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/store/repos/replica/PolarisDatabaseReplicaRepository.java
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/store/repos/replica/PolarisDatabaseReplicaRepository.java
@@ -1,11 +1,14 @@
 package com.netflix.metacat.connector.polaris.store.repos.replica;
 
+import com.netflix.metacat.connector.polaris.store.entities.PolarisDatabaseEntity;
 import com.netflix.metacat.connector.polaris.store.repos.primary.PolarisDatabaseCustomRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * JPA repository implementation for storing PolarisDatabaseEntity.
  */
 @Repository
-public interface PolarisDatabaseReplicaRepository extends PolarisDatabaseCustomRepository {
+public interface PolarisDatabaseReplicaRepository
+    extends JpaRepository<PolarisDatabaseEntity, String>, PolarisDatabaseCustomRepository {
 }
--- metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/store/repos/replica/PolarisTableReplicaRepository.java
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/store/repos/replica/PolarisTableReplicaRepository.java
@@ -1,12 +1,16 @@
 package com.netflix.metacat.connector.polaris.store.repos.replica;
 
+import com.netflix.metacat.connector.polaris.store.entities.PolarisDatabaseEntity;
+import com.netflix.metacat.connector.polaris.store.entities.PolarisTableEntity;
 import com.netflix.metacat.connector.polaris.store.repos.primary.PolarisTableCustomRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * JPA repository implementation for storing PolarisTableEntity.
  */
 @Repository
-public interface PolarisTableReplicaRepository extends PolarisTableCustomRepository {
+public interface PolarisTableReplicaRepository
+    extends JpaRepository<PolarisTableEntity, String>, PolarisTableCustomRepository {
 
 }
