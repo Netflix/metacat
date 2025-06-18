932dfda7550db2ceb462b3ccbecc16ee9dd14406
Yingjian Wu <yingjianw@netflix.com>
2025-06-18 09:28:41 -0700

    asda

--- metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/PolarisConnectorFactory.java
+++ metacat-connector-polaris/src/main/java/com/netflix/metacat/connector/polaris/PolarisConnectorFactory.java
@@ -9,6 +9,7 @@ import com.netflix.metacat.common.server.connectors.SpringConnectorFactory;
 import com.netflix.metacat.connector.polaris.configs.PolarisConnectorConfig;
 import com.netflix.metacat.connector.polaris.configs.PolarisPersistenceConfig;
 import com.netflix.metacat.connector.polaris.configs.PolarisPersistenceReaderConfig;
+import com.netflix.metacat.connector.polaris.configs.PolarisStoreConfig;
 import org.springframework.core.env.MapPropertySource;
 
 import java.util.Collections;
@@ -30,7 +31,9 @@ class PolarisConnectorFactory extends SpringConnectorFactory {
     ) {
         super(infoConverter, connectorContext);
         super.registerClazz(PolarisConnectorConfig.class,
-                PolarisPersistenceConfig.class, PolarisPersistenceReaderConfig.class);
+                PolarisPersistenceConfig.class,
+            PolarisPersistenceReaderConfig.class,
+            PolarisStoreConfig.class);
         super.addEnvProperties(new MapPropertySource(
                 "polaris_connector", Collections.unmodifiableMap(connectorContext.getConfiguration())));
         super.refresh();
