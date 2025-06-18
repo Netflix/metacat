eabf5ec9f51dcd9636697fbdfbcf6f2e0c5dfabd
Yingjian Wu <yingjianw@netflix.com>
2025-06-17 22:09:15 -0700

    Asda

--- metacat-connector-hive/src/test/groovy/com/netflix/metacat/connector/hive/sql/DirectSqlDatabaseSpec.groovy
+++ metacat-connector-hive/src/test/groovy/com/netflix/metacat/connector/hive/sql/DirectSqlDatabaseSpec.groovy
@@ -40,7 +40,7 @@ import spock.lang.Specification
  * @since 1.3.0
  */
 class DirectSqlDatabaseSpec extends Specification {
-    def config = new DefaultConfigImpl(new MetacatProperties())
+    def config = new DefaultConfigImpl(new MetacatProperties(null))
     def registry = new NoopRegistry()
     def context = DataDtoProvider.newContext(config, null)
     def metric = new HiveConnectorFastServiceMetric(registry)
